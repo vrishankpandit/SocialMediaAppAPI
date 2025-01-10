d20e166468d80b02d0ef08b5d9c25a60c6bf4f2c
Vishu <vrishankpandit1344@gmail.com>
2025-01-10 10:52:25 +0530

    add Exception Handling
    Created customException implements RuntimeException which throws a error
    message and a Custom HttpStatus code

--- restful-web-services/src/main/java/com/example/resfulwebservices/restful_web_services/user/UserResource.java
+++ restful-web-services/src/main/java/com/example/resfulwebservices/restful_web_services/user/UserResource.java
@@ -27,7 +27,10 @@ public class UserResource {
 	
 	@GetMapping("/users/{id}")
 	public User findUserById(@PathVariable int id){
-		return service.findOne(id);
+		User user = service.findOne(id);
+		if(user==null)
+			throw new UserNotFoundException("id:" + id);
+		return user;
 	}
 	
 	@PostMapping("/users")
