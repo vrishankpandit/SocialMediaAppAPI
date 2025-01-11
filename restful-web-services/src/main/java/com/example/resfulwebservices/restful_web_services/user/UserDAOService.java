b0e0550a3dc1cf8f6519d17323e458d677a13b46
Vishu <vrishankpandit1344@gmail.com>
2025-01-11 19:37:00 +0530

    add delete method

--- restful-web-services/src/main/java/com/example/resfulwebservices/restful_web_services/user/UserDAOService.java
+++ restful-web-services/src/main/java/com/example/resfulwebservices/restful_web_services/user/UserDAOService.java
@@ -26,6 +26,12 @@ public class UserDAOService {
 		return users.stream().filter(predicate).findFirst().orElse(null);
 	}
 	
+	public void deleteById(int id) {
+		Predicate<? super User> predicate = user -> user.getId().equals(id); 
+		users.removeIf(predicate);
+	}
+	
+	
 	public User save(User user) {
 		user.setId(++userCount);
 		users.add(user);
