3305aa8d122d1f12bf0cc8e31073e3e6cc01b808
Vishu <vrishankpandit1344@gmail.com>
2025-01-12 21:59:36 +0530

    add Internalization
    Displaying the message in different languages  using accept-language
    header

--- restful-web-services/src/main/java/com/example/resfulwebservices/restful_web_services/helloWorld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/example/resfulwebservices/restful_web_services/helloWorld/HelloWorldController.java
@@ -1,11 +1,21 @@
 package com.example.resfulwebservices.restful_web_services.helloWorld;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+	
+	private MessageSource messageSource;
+	
+	public HelloWorldController(MessageSource messageSource) {
+		this.messageSource=messageSource;
+	}
 	
 	@GetMapping(path="hello-world")
 	public String helloWorld() {
@@ -22,4 +32,11 @@ public class HelloWorldController {
 		return new HelloWorldBean(String.format("HelloWorld %s",name));
 	}
 	
+	@GetMapping(path="/hello-world-internalization")
+	public String helloWorldinternalization() {
+		
+		Locale locale=LocaleContextHolder.getLocale();
+		return messageSource.getMessage("good.morning.message", null, "Default message", locale);
+//		return "Hello-World-V2";
+	}
 }
